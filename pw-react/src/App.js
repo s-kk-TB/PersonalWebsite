0b807b25fa6bf111834ed555a54e5db495d9d046
s-kk-TB <silas.K.keru@gmail.com>
2024-06-18 20:22:33 +0400

    Base files cleaned up. Started section sizing

--- pw-react/src/App.js
+++ pw-react/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main-page">
+      <div id='header-section' className='debug-section'>
+        <div id="header-name-section">
+          <img></img><span>TrolledBartholomew</span>
+        </div>
+        <div id="header-skills-section">
+          <span>Skills</span><img></img>
+        </div>
+      </div>
+      <div id='top-section' className='debug-section'>
+        top section
+      </div>
+      <div id='middle-section' className='debug-section'>
+        <div className='middle-nav-section'>
+          <button>Mods</button>
+          <button>3D Projects</button>
+          <button>Gallery</button>
+        </div>
+      </div>
+      <div id='bottom-section' className='debug-section'>
+        <div className='bottom-info-card'>
+          <span>
+            This section showcases 3D projects created during my free
+            time that are just for posting on social media
+          </span>
+        </div>
+      </div>
+      <div id='footer-section' className='debug-section'>
+        footer section
+      </div>
     </div>
   );
 }
