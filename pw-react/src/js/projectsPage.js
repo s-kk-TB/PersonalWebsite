e3c29b96e3652a5e51aff046e923dbf032b50c4a
s-kk-TB <silas.K.keru@gmail.com>
2024-06-20 15:42:37 +0400

    Cleaned up nav button styling change.

--- pw-react/src/js/projectsPage.js
+++ pw-react/src/js/projectsPage.js
@@ -3,45 +3,35 @@ export default function getNavBttnStyle(picked,img,imgDefault){
     const navBttns = document.getElementsByClassName('middle-nav-bttns');
     const navImgs = document.getElementsByClassName('middle-nav-imgs');
 
+    function updateNavBttn(x){
+        navBttns[x].style.backgroundColor = '#400207';
+        navBttns[x].style.color = '#FDFFFF';
+        navBttns[x].style.fontFamily = 'source-sans-pro-black';
+        navImgs[x].src = img;
+        navImgs[x].style.transition = '0.25s ease-in-out';
+        navBttns[x].style.transition = '0.25s ease-in-out';
+    }
+
     for(let i=0; i<3; i++){
         navBttns[i].style.backgroundColor = '#FDFFFF';
         navBttns[i].style.color = '#010109';
         navBttns[i].style.fontFamily = 'source-sans-pro-light';
-        navBttns[i].zIndex = '1';
         navImgs[i].src = imgDefault[i];
     }
     switch(picked){
         /* MODS */
         case 0:
-            navBttns[0].style.backgroundColor = '#400207';
-            navBttns[0].style.color = '#FDFFFF';
-            navBttns[0].style.fontFamily = 'source-sans-pro-black';
-            navBttns[0].style.transition = '0.25s ease-in-out';
-            navImgs[0].src = img;
-            navBttns[0].zIndex = '999';
-            // navImgs[0].src = modsFilled;
+            updateNavBttn(0);
             break;
         
         /* PROJECTS */
         case 1:
-            navBttns[1].style.backgroundColor = '#400207';
-            navBttns[1].style.color = '#FDFFFF';
-            navBttns[1].style.fontFamily = 'source-sans-pro-black';
-            navBttns[1].zIndex = '999';
-            navImgs[1].src = img;
-            navBttns[1].style.transition = '0.25s ease-in-out';
-            // navImgs[2].src = projectFilled;
+            updateNavBttn(1);
             break;
 
         /* GALLERY */
         case 2:
-            navBttns[2].style.backgroundColor = '#400207';
-            navBttns[2].style.color = '#FDFFFF';
-            navBttns[2].style.fontFamily = 'source-sans-pro-black';
-            navBttns[2].zIndex = '999';
-            navImgs[2].src = img;
-            navBttns[2].style.transition = '0.25s ease-in-out';
-            // navImgs[2].src = galleryFilled;
+            updateNavBttn(2);
             break;
     }
 }
