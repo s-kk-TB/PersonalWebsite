9fba35e89357cdd2020f6cda324abb3d9bf8e6a9
s-kk-TB <silas.K.keru@gmail.com>
2024-07-01 09:35:43 +0400

    Added skills drop down

--- pw-react/src/App.js
+++ pw-react/src/App.js
@@ -53,7 +53,23 @@ function App() {
           <img src={tbProfile} alt='Profile icon'></img><span>TrolledBartholomew</span>
         </div>
         <div id="header-skills-section">
-          <span>Skills</span><img src={dwnArrowUnfilled} alt='Down Arrow'></img>
+          <select>
+            <option selected='selected' disabled hidden>Skills</option>
+            <option disabled>HTML</option>
+            <option disabled>CSS</option>
+            <option disabled>Javascript</option>
+            <option disabled>NodeJs</option>
+            <option disabled>ReactJs</option>
+            <option disabled>React Three Fiber</option>
+            <option disabled>Git</option>
+            <option disabled>Typescript</option>
+            <option disabled>Java</option>
+            <option disabled>PHP</option>
+            <option disabled>Blender</option>
+            <option disabled>Figma</option>
+            <option disabled>VS code</option>
+            <option disabled>Microsoft Office Suite</option>
+          </select>
         </div>
       </div>
       <div id='top-section' className='debug-section'>
