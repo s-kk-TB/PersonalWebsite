6f540de8adc6a1ac905144f86160573c3569c241
s-kk-TB <silas.K.keru@gmail.com>
2024-07-02 11:27:25 +0400

    Footer component js/css cleanup

--- pw-react/src/components/Footer.js
+++ pw-react/src/components/Footer.js
@@ -1,15 +1,21 @@
 export default function Footer({instagramUnfilled,twitterUnfilled,redditUnfilled}){
     return(
-        <div id='footer-section' className='debug-section'>
+      <div id='footer-section' className='debug-section'>
         <div id='footer-icons-unfilled'>
+
+          {/* INSTAGRAM */}
           <a href='https://www.instagram.com/trolledbartholomew/' target='_blank' rel='noreferrer'>
-            <img id='img-insta' src={instagramUnfilled} alt='instagram-unfilled'></img>
+            <img id='footer-img-insta' src={instagramUnfilled} alt='instagram-unfilled'></img>
           </a>
+
+          {/* REDDIT */}
           <a href='https://www.reddit.com/user/TrolledBartholomew/' target='_blank' rel='noreferrer'>
-            <img id='img-reddit' src={redditUnfilled} alt='reddit-unfilled'></img>
+            <img id='footer-img-reddit' src={redditUnfilled} alt='reddit-unfilled'></img>
           </a>
+
+          {/* TWITTER */}
           <a href='https://x.com/TrolledBartholo' target='_blank' rel='noreferrer'>
-            <img id='img-twitter' src={twitterUnfilled} alt='twitter-unfilled'></img>
+            <img id='footer-img-twitter' src={twitterUnfilled} alt='twitter-unfilled'></img>
           </a>
         </div>
       </div>
